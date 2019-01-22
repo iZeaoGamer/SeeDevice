1cf56eb6621b963f4728faf436317127fd03c688
iZeaoGamer <jameshayward14@icloud.com>
2019-01-22 09:40:28 +0000

    Update TheTask.php

--- src/SeeDevice/TheTask.php
+++ src/SeeDevice/TheTask.php
@@ -13,7 +13,7 @@ class TheTask extends Task{
 		foreach(Server::getInstance()->getOnlinePlayers() as $player){
 			$player->setNameTagVisible();
 			#Thanks to virvolta
-			$player->setScoreTag("§f\n[§c".$player->getHealth()."§f/20]\n§5".$this->api->getUos($player));
+			$player->setScoreTag("§f\n[§c".$player->getHealth()."§f/".$player->getMaxHealth()."]\n§d".$this->api->getUos($player));
 		}
 	}
 }
